80058aae6b67f9c287c4ccaba8a6a46dcd43088d
Hosea Barriga <hor.barr1ga@gmail.com>
2022-11-20 01:30:14 +0800

    UI

--- horeb-cipher/src/App.tsx
+++ horeb-cipher/src/App.tsx
@@ -1,39 +1,139 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./App.css";
 import useHorebCipher from "./hooks/useCipher";
+import TextareaAutosize from "react-textarea-autosize";
+import { HiOutlineUpload } from "react-icons/hi";
 
 const App = () => {
-  const [plainText, setPlainText] = useState<string>("");
-  const [cipherText, setCipherText] = useState<string>("");
-  const { encrypt, decrypt } = useHorebCipher();
+  const [text, setText] = useState<string>("");
+  const [resultText, setResultText] = useState<string>("");
+  const {
+    encrypt,
+    decrypt,
+    setRotationI,
+    setRotationII,
+    setRotationIII,
+    setRotationIV,
+    setRotationV,
+  } = useHorebCipher();
+  const [keyFocus, setKeyFocus] = useState<number>(1);
 
-  const updatePlainText = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPlainText(e.target.value);
+  const plainTextRef = useRef<HTMLTextAreaElement | null>(null);
+  const cipherTextRef = useRef<HTMLTextAreaElement | null>(null);
+
+  const updatePlainText = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value);
   };
 
   const encryptClicked = () => {
-    const result = encrypt(plainText);
-    setPlainText(result);
+    const result = encrypt(text);
+    setResultText(result);
   };
 
   const decryptClicked = () => {
-    const result = decrypt(plainText);
-    setPlainText(result);
+    const result = decrypt(text);
+    setResultText(result);
   };
+
+  const resetFields = () => {
+    setResultText("");
+    setText("");
+  };
+
+  const updateKeyFocus = (key: number) => {
+    setKeyFocus(key);
+  };
+
+  const updateKeyI = (e: React.ChangeEvent<HTMLInputElement>) => {};
+  const updateKeyII = (e: React.ChangeEvent<HTMLInputElement>) => {};
+  const updateKeyIII = (e: React.ChangeEvent<HTMLInputElement>) => {};
+  const updateKeyIV = (e: React.ChangeEvent<HTMLInputElement>) => {};
+  const updateKeyV = (e: React.ChangeEvent<HTMLInputElement>) => {};
 
   return (
     <div className="App">
-      Plain Text: {plainText}
-      <br></br>
-      Cipher Text: {cipherText}
-      <br></br>
-      <input type="text" onChange={(e) => updatePlainText(e)}></input>
-      <button type="button" onClick={encryptClicked}>
-        Encrypt
-      </button>
-      <button type="button" onClick={decryptClicked}>
-        Decrypt
-      </button>
+      <div className="container">
+        {/* <label>Plain Text </label> */}
+        <div className="input">
+          <TextareaAutosize
+            ref={plainTextRef}
+            onChange={(e) => updatePlainText(e)}
+            placeholder="Whats on your mind?"
+            className="form-control"
+            minRows={5}
+            maxRows={15}
+          />
+          <label htmlFor="upload-photo" className="upload">
+            <HiOutlineUpload></HiOutlineUpload>
+          </label>
+          <input
+            type="file"
+            id="upload-photo"
+            // onChange={onSelectFile}
+          />
+        </div>
+        <span className="keys">
+          <div className={keyFocus === 1 ? "key key-focus" : "key"}>
+            <input
+              className="form-control"
+              type="text"
+              onFocus={() => updateKeyFocus(1)}
+              onChange={(e) => updateKeyI(e)}
+            ></input>
+          </div>
+          <div className={keyFocus === 2 ? "key key-focus" : "key"}>
+            <input
+              className="form-control"
+              type="text"
+              onFocus={() => updateKeyFocus(2)}
+              onChange={(e) => updateKeyI(e)}
+            ></input>
+          </div>
+          <div className={keyFocus === 3 ? "key key-focus" : "key"}>
+            <input
+              className="form-control"
+              type="text"
+              onFocus={() => updateKeyFocus(3)}
+              onChange={(e) => updateKeyI(e)}
+            ></input>
+          </div>
+          <div className={keyFocus === 4 ? "key key-focus" : "key"}>
+            <input
+              className="form-control"
+              type="text"
+              onFocus={() => updateKeyFocus(4)}
+              onChange={(e) => updateKeyI(e)}
+            ></input>
+          </div>
+          <div className={keyFocus === 5 ? "key key-focus" : "key"}>
+            <input
+              className="form-control"
+              type="text"
+              onFocus={() => updateKeyFocus(5)}
+              onChange={(e) => updateKeyI(e)}
+            ></input>
+          </div>
+        </span>
+        <span className="horizontal-interactions">
+          <button
+            type="button"
+            className="primary-btn"
+            onClick={encryptClicked}
+          >
+            Encrypt
+          </button>
+          <button
+            type="button"
+            className="secondary-btn"
+            onClick={decryptClicked}
+          >
+            Decrypt
+          </button>
+        </span>
+        <button type="button" className="secondary-btn" onClick={resetFields}>
+          Reset
+        </button>
+      </div>
     </div>
   );
 };
--- horeb-cipher/src/hooks/useCipher.ts
+++ horeb-cipher/src/hooks/useCipher.ts
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const abcdef = "abcdefghijklmnopqrstuvwxyz";
 const colemak = "qwfpgjluyarstdhneiozxcvbkm";
@@ -9,148 +9,191 @@ const MAX_ALPHABET = 26;
 const HALF_ALPHABET = MAX_ALPHABET / 2;
 
 const isAlphabet = (character: string): boolean => {
-    if (!/[^a-zA-Z]/.test(character)) {
-        return true;
-    }
+  if (!/[^a-zA-Z]/.test(character)) {
+    return true;
+  }
 
-    return false;
+  return false;
+};
+
+const mod = (n: number, m: number): number => {
+  return ((n % m) + m) % m;
 };
 
 const ACTIONS: {
-    ENCRYPT: number,
-    DECRYPT: number,
+  ENCRYPT: number;
+  DECRYPT: number;
 } = {
-    ENCRYPT: 1,
-    DECRYPT: 2,
-}
+  ENCRYPT: 1,
+  DECRYPT: 2,
+};
 
 const useCipher = () => {
-    var action = 0;
-    const [rotationI, setRotationI] = useState<number>(3);
-    const [rotationII, setRotationII] = useState<number>(8);
-    const [rotationIII, setRotationIII] = useState<number>(4);
-    const [rotationIV, setRotationIV] = useState<number>(19);
-    const [rotationV, setRotationV] = useState<number>(10);
+  var action = 0;
+  const [rotationI, setRotationI] = useState<number>(3);
+  const [rotationII, setRotationII] = useState<number>(8);
+  const [rotationIII, setRotationIII] = useState<number>(4);
+  const [rotationIV, setRotationIV] = useState<number>(19);
+  const [rotationV, setRotationV] = useState<number>(10);
 
-    const setKey = (key: string): void => {
-        
+  const setKey = (key: string): void => {};
+
+  const formatCharacter = (character: string): string => {
+    if (character === character.toUpperCase()) {
+      return character.toUpperCase();
+    }
+    if (character === character.toLowerCase()) {
+      return character.toLowerCase();
     }
 
-    const formatCharacter = (character: string): string => {
-        if (character === character.toUpperCase()) {
-            return character.toUpperCase();
-        }
-        if (character === character.toLowerCase()) {
-            return character.toLowerCase();
-        }
+    return character;
+  };
 
-        return character;
-    };
+  const rotorI = (character: string): string => {
+    const idx = abcdef.indexOf(character.toLowerCase());
+    const rotorIChar =
+      action === ACTIONS.ENCRYPT
+        ? abcdef[mod(idx + rotationI, MAX_ALPHABET)]
+        : abcdef[mod(idx - rotationI, MAX_ALPHABET)];
 
-    const rotorI = (character: string): string => {
-        const idx = abcdef.indexOf(character.toLowerCase());
-        const rotorIChar = action === ACTIONS.ENCRYPT ? abcdef[(idx + rotationI) % MAX_ALPHABET] : abcdef[(idx - rotationI) % MAX_ALPHABET];
-        console.log(rotorIChar);
+    // return
+    const rotorIIChar = rotorII(rotorIChar);
+    const rotorIICharIdx = abcdef.indexOf(rotorIIChar);
+    const returnRotorIChar =
+      action === ACTIONS.ENCRYPT
+        ? abcdef[mod(rotorIICharIdx + rotationI, MAX_ALPHABET)]
+        : abcdef[mod(rotorIICharIdx - rotationI, MAX_ALPHABET)];
+    // console.log(returnRotorIChar);
 
-        // return
-        const rotorIIChar = rotorII(rotorIChar);
-        const rotorIICharIdx = abcdef.indexOf(rotorIIChar);
-        const returnRotorIChar = action === ACTIONS.ENCRYPT ?
-            abcdef[(rotorIICharIdx + rotationI) % MAX_ALPHABET] : abcdef[(rotorIICharIdx - rotationI) % MAX_ALPHABET];
-        console.log(returnRotorIChar);
+    return formatCharacter(returnRotorIChar);
+  };
 
-        return formatCharacter(returnRotorIChar);
-    };
+  const rotorII = (character: string): string => {
+    const idx = colemak.indexOf(character.toLowerCase());
+    console.log(idx);
+    const rotorIIChar =
+      action === ACTIONS.ENCRYPT
+        ? colemak[mod(idx + rotationII, MAX_ALPHABET)]
+        : colemak[mod(idx - rotationII, MAX_ALPHABET)];
+    console.log((idx - rotationII) % MAX_ALPHABET);
+    console.log(colemak[(idx - rotationII) % MAX_ALPHABET]);
+    console.log(rotorIIChar);
 
-    const rotorII = (character: string): string => {
-        const idx = colemak.indexOf(character.toLowerCase());
-        const rotorIIChar = action === ACTIONS.ENCRYPT ? colemak[(idx + rotationII) % MAX_ALPHABET] : colemak[(idx - rotationII) % MAX_ALPHABET];
-        console.log(rotorIIChar);
+    // return
+    const rotorIIIChar = rotorIII(rotorIIChar);
+    const rotorIIICharIdx = colemak.indexOf(rotorIIIChar);
+    const returnRotorIIChar =
+      action === ACTIONS.ENCRYPT
+        ? colemak[mod(rotorIIICharIdx + rotationII, MAX_ALPHABET)]
+        : colemak[mod(rotorIIICharIdx - rotationII, MAX_ALPHABET)];
+    // console.log(returnRotorIIChar);
+    return returnRotorIIChar;
+  };
 
-        // return
-        const rotorIIIChar = rotorIII(rotorIIChar);
-        const rotorIIICharIdx = colemak.indexOf(rotorIIIChar);
-        const returnRotorIIChar = action === ACTIONS.ENCRYPT ?
-            colemak[(rotorIIICharIdx + rotationII) % MAX_ALPHABET] : colemak[(rotorIIICharIdx - rotationII) % MAX_ALPHABET];
-        console.log(returnRotorIIChar);
-        return returnRotorIIChar;
-    };
+  const rotorIII = (character: string): string => {
+    console.log(character);
+    const idx = azerty.indexOf(character.toLowerCase());
+    const rotorIIIChar =
+      action === ACTIONS.ENCRYPT
+        ? azerty[mod(idx + rotationIII, MAX_ALPHABET)]
+        : azerty[mod(idx - rotationIII, MAX_ALPHABET)];
+    console.log(rotorIIIChar);
 
-    const rotorIII = (character: string): string => {
-        const idx = azerty.indexOf(character.toLowerCase());
-        const rotorIIIChar = action === ACTIONS.ENCRYPT ? azerty[(idx + rotationIII) % MAX_ALPHABET] : azerty[(idx - rotationIII) % MAX_ALPHABET];
-        console.log(rotorIIIChar);
+    // return
+    const rotorIVChar = rotorIV(rotorIIIChar);
+    const rotorIVCharIdx = azerty.indexOf(rotorIVChar);
+    const returnRotorIIIChar =
+      action === ACTIONS.ENCRYPT
+        ? azerty[mod(rotorIVCharIdx + rotationIII, MAX_ALPHABET)]
+        : azerty[mod(rotorIVCharIdx - rotationIII, MAX_ALPHABET)];
+    // console.log(returnRotorIIIChar);
+    return returnRotorIIIChar;
+  };
 
-        // return
-        const rotorIVChar = rotorIV(rotorIIIChar);
-        const rotorIVCharIdx = azerty.indexOf(rotorIVChar);
-        const returnRotorIIIChar = action === ACTIONS.ENCRYPT ?
-            azerty[(rotorIVCharIdx + rotationIII) % MAX_ALPHABET] : azerty[(rotorIVCharIdx - rotationIII) % MAX_ALPHABET];
-        console.log(returnRotorIIIChar);
-        return returnRotorIIIChar;
-    };
+  const rotorIV = (character: string): string => {
+    const idx = qwerty.indexOf(character.toLowerCase());
+    const rotorIVChar =
+      action === ACTIONS.ENCRYPT
+        ? qwerty[mod(idx + rotationIV, MAX_ALPHABET)]
+        : qwerty[mod(idx - rotationIV, MAX_ALPHABET)];
+    // console.log(rotorIVChar);
 
-    const rotorIV = (character: string): string => {
-        const idx = qwerty.indexOf(character.toLowerCase());
-        const rotorIVChar = action === ACTIONS.ENCRYPT ? qwerty[(idx + rotationIV) % MAX_ALPHABET] : qwerty[(idx - rotationIV) % MAX_ALPHABET];
-        console.log(rotorIVChar);
+    // return
+    const rotorVChar = rotorV(rotorIVChar);
+    const rotorVCharIdx = qwerty.indexOf(rotorVChar);
+    const returnRotorIVChar =
+      action === ACTIONS.ENCRYPT
+        ? qwerty[mod(rotorVCharIdx + rotationIV, MAX_ALPHABET)]
+        : qwerty[mod(rotorVCharIdx - rotationIV, MAX_ALPHABET)];
+    // console.log(returnRotorIVChar);
+    return returnRotorIVChar;
+  };
 
-        // return
-        const rotorVChar = rotorV(rotorIVChar);
-        const rotorVCharIdx = qwerty.indexOf(rotorVChar);
-        const returnRotorIVChar = action === ACTIONS.ENCRYPT ?
-            qwerty[(rotorVCharIdx + rotationIV) % MAX_ALPHABET] : qwerty[(rotorVCharIdx - rotationIV) % MAX_ALPHABET];
-        console.log(returnRotorIVChar);
-        return returnRotorIVChar;
-    };
+  const rotorV = (character: string): string => {
+    var idx = dvorak.indexOf(character.toLowerCase());
+    const rotorVChar =
+      action === ACTIONS.ENCRYPT
+        ? dvorak[mod(idx + rotationV, MAX_ALPHABET)]
+        : dvorak[mod(idx - rotationV, MAX_ALPHABET)];
+    // console.log(rotorVChar);
 
-    const rotorV = (character: string): string => {
-        var idx = dvorak.indexOf(character.toLowerCase());
-        const rotorVChar = action === ACTIONS.ENCRYPT ? dvorak[(idx + rotationV) % MAX_ALPHABET] : dvorak[(idx - rotationV) % MAX_ALPHABET];
-        console.log(rotorVChar);
+    // return
+    console.log("second pass");
+    idx = dvorak.indexOf(rotorVChar.toLowerCase());
+    idx =
+      action === ACTIONS.ENCRYPT
+        ? mod(idx + HALF_ALPHABET, MAX_ALPHABET)
+        : mod(idx - HALF_ALPHABET, MAX_ALPHABET);
+    const returnRotorVChar =
+      action === ACTIONS.ENCRYPT
+        ? dvorak[mod(idx + rotationV, MAX_ALPHABET)]
+        : dvorak[mod(idx - rotationV, MAX_ALPHABET)];
+    // console.log(returnRotorVChar);
+    return returnRotorVChar;
+  };
 
-        // return
-        console.log("second pass");
-        idx = dvorak.indexOf(rotorVChar.toLowerCase());
-        idx = action === ACTIONS.ENCRYPT ? (idx + HALF_ALPHABET) % MAX_ALPHABET : (idx - HALF_ALPHABET) % MAX_ALPHABET;
-        const returnRotorVChar = action === ACTIONS.ENCRYPT ? dvorak[(idx + rotationV) % MAX_ALPHABET] : dvorak[(idx - rotationV) % MAX_ALPHABET];
-        console.log(returnRotorVChar);
-        return returnRotorVChar;
-    };
+  const horebCipher = (text: string): string => {
+    var result = "";
+    for (var textIdx = 0; textIdx < text.length; textIdx++) {
+      const curr = text[textIdx];
+      console.log(curr);
 
-    const horebCipher = (text: string): string => {
-        var tempCipherText = "";
-        for (var textIdx = 0; textIdx < text.length; textIdx++) {
-            const curr = text[textIdx];
-
-            if (isAlphabet(curr)) {
-                const cipherCharacter = rotorI(curr);
-                tempCipherText = tempCipherText.concat(cipherCharacter);
-                console.log(curr);
-            } else {
-                tempCipherText = tempCipherText.concat(curr);
-            }
-        }
-
-        return tempCipherText;
+      if (isAlphabet(curr)) {
+        const cipherCharacter = rotorI(curr);
+        result = result.concat(cipherCharacter);
+      } else {
+        result = result.concat(curr);
+      }
     }
 
-    // run encryption
-    const encrypt = (text: string): string => {
-        action = ACTIONS.ENCRYPT;
-        return horebCipher(text);
-    };
+    return result;
+  };
 
-    // run encryption
-    const decrypt = (text: string): string => {
-        action = ACTIONS.DECRYPT;
-        return horebCipher(text);
-    };
+  // run encryption
+  const encrypt = (text: string): string => {
+    action = ACTIONS.ENCRYPT;
+    console.log(action);
+    return horebCipher(text);
+  };
 
-    return {
-        encrypt,
-        decrypt,
-    }
-}
+  // run encryption
+  const decrypt = (text: string): string => {
+    console.log(text);
+    action = ACTIONS.DECRYPT;
+    console.log(action);
 
-export default useCipher
+    return horebCipher(text);
+  };
+
+  return {
+    encrypt,
+    decrypt,
+    setRotationI,
+    setRotationII,
+    setRotationIII,
+    setRotationIV,
+    setRotationV,
+  };
+};
+
+export default useCipher;
