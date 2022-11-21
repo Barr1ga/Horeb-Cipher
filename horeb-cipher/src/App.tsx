85b56783b8c4730f0aad27cda03ab032d339a66b
Barriga <hor.barr1ga@gmail.com>
2022-11-21 19:21:44 +0800

    commit

--- horeb-cipher/src/App.tsx
+++ horeb-cipher/src/App.tsx
@@ -1,14 +1,15 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./App.css";
 import useHorebCipher from "./hooks/useCipher";
 import TextareaAutosize from "react-textarea-autosize";
+import Logo from "./assets/horeb-cipher-logo.svg";
 import {
   HiChevronDown,
   HiChevronUp,
   HiOutlineDownload,
   HiOutlineUpload,
   HiPencil,
-  HiQrcode,
+  HiOutlineQrcode,
 } from "react-icons/hi";
 import { IconContext } from "react-icons";
 import { mod } from "./utils/mod";
@@ -25,15 +26,16 @@ const filename = "cipher-result";
 
 const App = () => {
   const [text, setText] = useState<string>("");
+  const [encrypted, setEncrypted] = useState<boolean>(false);
   const {
     encrypt,
     decrypt,
-    setRotationI,
     rotationI,
     rotationII,
     rotationIII,
     rotationIV,
     rotationV,
+    setRotationI,
     setRotationII,
     setRotationIII,
     setRotationIV,
@@ -43,17 +45,24 @@ const App = () => {
   const textRef = useRef<HTMLTextAreaElement | null>(null);
   const fileRef = useRef<HTMLInputElement | null>(null);
 
+  useEffect(() => {
+    document.title = "Horeb Cipher";
+  }, []);
+
   const updatePlainText = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setText(e.target.value);
   };
 
   const encryptClicked = () => {
+    setEncrypted(true);
+
     const result = encrypt(text);
     setText(result);
     textRef.current!.value = result;
   };
 
   const decryptClicked = () => {
+    setEncrypted(false);
     const result = decrypt(text);
     setText(result);
     textRef.current!.value = result;
@@ -63,6 +72,11 @@ const App = () => {
     setText("");
     textRef.current!.value = "";
     fileRef.current!.value = "";
+    setRotationI(0);
+    setRotationII(0);
+    setRotationIII(0);
+    setRotationIV(0);
+    setRotationV(0);
   };
 
   const incrementKeyI = () => {
@@ -80,7 +94,6 @@ const App = () => {
   const incrementKeyV = () => {
     setRotationV((prev: number) => mod(prev + 1, MAX_ALPHABET));
   };
-
   const decrementKeyI = () => {
     setRotationI((prev: number) => mod(prev - 1, MAX_ALPHABET));
   };
@@ -129,9 +142,9 @@ const App = () => {
       <div className="container">
         <div className="logo">
           <div className="icon">
-            <HiQrcode></HiQrcode>
+            <img src={Logo} alt={"horeb-cipher-logo"} />
           </div>
-          <h2>horeb-cipher</h2>
+          <h2>horeb-{!encrypted ? "cipher" : "sdgqmi"}</h2>
         </div>
 
         {/* <label>Plain Text </label> */}
@@ -204,13 +217,29 @@ const App = () => {
             minRows={5}
             maxRows={15}
           />
-          <label
-            className={text.length === 0 ? "filename disabled" : "filename"}
-            onClick={downloadResult}
-          >{`${filename}.txt`}</label>
-          <label htmlFor="upload-photo" className="upload">
-            <HiOutlineUpload></HiOutlineUpload>
-          </label>
+          <span className="filename">
+            <div className="tooltip-container">
+              <label
+                className={text.length === 0 ? "blurred" : ""}
+                onClick={downloadResult}
+              >{`${filename}.txt`}</label>
+              <div className="tooltip">
+                {"Download File"}
+                <div className="arrow-down"></div>
+              </div>
+            </div>
+          </span>
+          <span className="upload">
+            <div className="tooltip-container">
+              <label htmlFor="upload-photo">
+                <HiOutlineUpload></HiOutlineUpload>
+              </label>
+              <div className="tooltip">
+                {"Upload File"}
+                <div className="arrow-down"></div>
+              </div>
+            </div>
+          </span>
           <input
             type="file"
             id="upload-photo"
@@ -241,6 +270,7 @@ const App = () => {
         <button type="button" className="secondary-btn" onClick={resetFields}>
           Reset
         </button>
+        
       </div>
     </div>
   );
