252b7469467cb1f1ef50abe82c01d3baa1b26d95
Barriga <hor.barr1ga@gmail.com>
2022-11-25 23:54:44 +0800

    commit

--- horeb-cipher/src/hooks/useRsa.ts
+++ horeb-cipher/src/hooks/useRsa.ts
@@ -26,6 +26,7 @@ const useRsa = () => {
   var privateKey = 0;
   var product = 0;
   var totient = 0;
+  var padding = 0;
 
   const rsaConstructor = (
     rotationI: number,
@@ -34,12 +35,13 @@ const useRsa = () => {
     rotationIV: number,
     rotationV: number
   ) => {
-    primeP = getPrime(rotationI + 1);
+    primeP = getPrime(rotationI);
     primeQ = getPrime(rotationII + 1);
     product = primeP * primeQ;
     totient = (primeP - 1) * (primeQ - 1);
     publicKey = setPublicKey(totient);
     privateKey = setPrivateKey(publicKey, totient);
+    padding = rotationIII;
 
     // primeP = 7;
     // primeQ = 19;
@@ -57,41 +59,72 @@ const useRsa = () => {
     console.log("product", product);
   };
 
-  const bigNumberProcess = (character: string): string => {
+  const bigNumberProcess = (curr: string): string => {
     // (char ^ key) % product
-    const idx = abcdef.indexOf(character);
+    const idx =
+      action === ACTIONS.ENCRYPT
+        ? abcdef.indexOf(curr) : parseInt(curr);
     const bigIdx = new BigNumber(idx);
+
+    console.log(idx);
 
     var bigKey =
       action === ACTIONS.ENCRYPT
-        ? new BigNumber(publicKey)
-        : new BigNumber(privateKey);
+        ? new BigNumber(privateKey)
+        : new BigNumber(publicKey);
 
     console.log(
       "result",
-      `(${bigIdx} ^ ${bigKey}) % ${product} = ${modulo(
-        bigIdx.exponentiatedBy(bigKey).modulo(product).toNumber(),
-        26
-      )}`
+      `(${bigIdx} ^ ${bigKey}) % ${product} = ${bigIdx.exponentiatedBy(bigKey).modulo(product).toNumber()}`
     );
 
-    return abcdef[
-      bigIdx.exponentiatedBy(bigKey).modulo(product).modulo(26).toNumber()
-    ];
+    if (action === ACTIONS.ENCRYPT) {
+      return bigIdx.exponentiatedBy(bigKey).modulo(product).toNumber().toString();
+    } else {
+      return abcdef[bigIdx.exponentiatedBy(bigKey).modulo(product).toNumber()];
+    }
   };
 
   const rsa = (text: string): string => {
-    console.log(text);
-    var result = "";
-    for (var textIdx = 0; textIdx < text.length; textIdx++) {
-      const curr = text[textIdx];
-      console.log("curr", curr);
 
-      if (isAlphabet(curr)) {
-        const resultCharacter = bigNumberProcess(curr);
-        result = result.concat(resultCharacter);
-      } else {
-        result = result.concat(curr);
+    // ab c
+    // 1 2 X3
+    // ab c 
+    var result = "";
+
+    if (action === ACTIONS.ENCRYPT) {
+      const length = text.length;
+
+      for (var textIdx = 0; textIdx < length; textIdx++) {
+        var curr = text[textIdx];
+
+        if (curr === ' ') {
+          result = result.concat(abcdef[padding].toUpperCase());
+        } else {
+          if (isAlphabet(curr)) {
+            result = result.concat(bigNumberProcess(curr));
+          }
+        }
+
+        if (textIdx !== length - 1) {
+          result = result.concat(' ');
+        }
+      }
+    }
+
+    if (action === ACTIONS.DECRYPT) {
+      const length = text.split(' ').length;
+
+      for (var textIdx = 0; textIdx < length; textIdx++) {
+        var curr = text.split(' ')[textIdx];
+
+        if (isAlphabet(curr)) {
+          result = result.concat(' ');
+        }
+
+        if (!isAlphabet(curr)) {
+          result = result.concat(bigNumberProcess(curr));
+        }
       }
     }
 
